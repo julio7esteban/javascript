4c07a0e8b6b2f4721398b934a806e8cbe9c2f4cf
“Julio” <jestobar@udlanet.ec>
2019-11-05 19:59:18 -0500

    Fin del platzi videos con hooks

---
+++ Reacjs/src/Containers/App.jsx
@@ -0,0 +1,67 @@
+import React,{useState,useEffect} from 'react';
+import Header from '../components/Header';
+import '../assets/styles/App.scss';
+import Search from  '../components/Search';
+import Categories from '../components/Categories'
+import Carousel from '../components/Carousel'
+import CarouselItem from '../components/CarouselItem'
+import Footer from '../components/Footer'
+import useInitialState  from '../hooks/useInitialState';
+const API = 'http://localhost:3000/initalState';
+
+const App=()=>{
+  
+    const initalState = useInitialState(API);
+    return(
+        <div className="App">
+            <Header />
+            <Search />
+            {
+                initalState.mylist.length > 0 &&
+                <Categories title="Mi Lista">
+                    <Carousel>
+                        {
+                            initalState.mylist.map(item =>
+                                <CarouselItem key={item.id}{...item} />
+
+                            )
+
+                        }
+
+                    </Carousel>
+                </Categories>
+
+            }
+           
+            <Categories title="Tendencias" >
+                <Carousel>
+                    {
+                        initalState.trends.map(item =>
+                            <CarouselItem key={item.id}{...item} />
+                            
+                            )
+
+                    }
+              
+              
+            </Carousel>
+            </Categories>
+            <Categories title="Originales de Platzi video">
+                <Carousel>
+                    {
+                        initalState.originals.map(item =>
+                            <CarouselItem key={item.id}{...item} />
+
+                        )
+
+                    }
+                    
+                </Carousel>
+            </Categories>
+            <Footer/>
+
+
+        </div>
+    );
+    }
+export default App;
